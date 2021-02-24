752b38da095d152af27cde94be5eecbf304499af
zhangkunhua <zhangkunhua@le.com>
2021-02-24 19:31:44 +0800

    state1 done

--- demo/src/views/AdvancedUse/index.vue
+++ demo/src/views/AdvancedUse/index.vue
@@ -2,18 +2,22 @@
     <div>
         <!-- <ToastDemo1/> -->
         <!-- <TabsDemo1/> -->
+        <!-- <ButtonDemo1/> -->
         <PopoverDemo1/>
     </div>
 </template>
 
 <script>
 
+// import ButtonDemo1 from './button-demo-1'
 // import ToastDemo1 from './toast-demo-1'
 // import TabsDemo1 from './tabs-demo-1'
 import PopoverDemo1 from './popover-demo-1'
 
 export default {
     components: {
+        // ToastDemo1
+        // ButtonDemo1
         PopoverDemo1
     },
     data() {

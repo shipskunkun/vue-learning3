a7c91b73ba5c928be99e72010c736a48b65b4628
zhangkunhua <zhangkunhua@le.com>
2021-03-30 17:54:14 +0800

    sticky update

--- demo/src/views/AdvancedUse/index.vue
+++ demo/src/views/AdvancedUse/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <!-- <ToastDemo1/> -->
-        <!-- <TabsDemo1/> -->
+        <TabsDemo1/>
         <!-- <ButtonDemo1/> -->
-        <PopoverDemo1/>
+        <!-- <PopoverDemo1/> -->
     </div>
 </template>
 
@@ -11,14 +11,15 @@
 
 // import ButtonDemo1 from './button-demo-1'
 // import ToastDemo1 from './toast-demo-1'
-// import TabsDemo1 from './tabs-demo-1'
-import PopoverDemo1 from './popover-demo-1'
+import TabsDemo1 from './tabs-demo-1'
+// import PopoverDemo1 from './popover-demo-1'
 
 export default {
     components: {
         // ToastDemo1
         // ButtonDemo1
-        PopoverDemo1
+        // PopoverDemo1
+        TabsDemo1
     },
     data() {
         return {
